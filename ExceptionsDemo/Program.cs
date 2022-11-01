265715d6f974e6c657d5f818decb7aceb0effd8a
NatalieDillon <112569645+NatalieDillon@users.noreply.github.com>
2022-11-01 14:03:29 +0000

    Add project files.

---
+++ ExceptionsDemo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace ExceptionsDemo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // Division(100, 0);
+            //ReadTextFile();
+
+            SimpleBankAccount bankAccount = new (100);
+            bankAccount.Withdraw(300);
+        }
+
+        public static void Division(int num1, int num2)
+        {
+            int result = 0;
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"Exception caught: {e}");
+            }
+            finally
+            {
+                Console.WriteLine($"Result is {result}");
+            }
+        }
+
+        public static void ReadTextFile()
+        {
+            string fileName = @"/Files/mytext.txt";
+            //string fileName = @"mytext.txt";
+
+            try
+            {
+                string allText = File.ReadAllText(fileName);
+                Console.Write(allText);
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Directory not found.");
+                Console.WriteLine($"Exception {e}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Couldn't read file for some other reason.");
+                Console.WriteLine($"Exception {ex}");
+            }
+           
+
+            
+        }
+
+
+        public static void WriteTextFile()
+        {
+            string fileName = $"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\TestException.txt";
+            int numTries = 1;
+
+            try
+            {
+                File.AppendAllLines(fileName, new[] { "No problem this time" });
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                Console.WriteLine($"Directory not found.");
+                Console.WriteLine($"Exception {e}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Couldn't read file for some other reason.");
+                Console.WriteLine($"Exception {ex}");
+            }
+
+
+
+        }
+    }
+}
---
+++ ExceptionsDemo/SimpleBankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsDemo
+{
+    public class SimpleBankAccount
+    {
+        public decimal Balance { get; private set; }
+        public decimal Overdraft { get; init; }
+
+        public SimpleBankAccount(decimal overdraft)
+        {
+            Overdraft = overdraft;
+            Balance = 0;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if (Math.Abs(Balance - amount) > Overdraft)
+            {
+                throw new InvalidOperationException("Overdraft exceeded");
+            }
+            Balance -= amount;
+        }
+        
+
+    }
+}
